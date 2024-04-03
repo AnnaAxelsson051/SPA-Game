92cc8a1bca0ed75be49c4bc4c94dbc145e7230e5
Anna Axelsson <a.axelsson51@gmail.com>
2024-04-03 16:25:10 +0200

    Add functionality for notification of new game

---
+++ Hubs/GameHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SPAGame.Hubs
+{
+    public class GameHub : Hub
+    {
+
+        //Sending message to connected
+        //clients, notifying them about new game
+
+        public async Task NotifyNewGame(string gameId)
+        {
+            await Clients.All.SendAsync("NewGame", gameId);
+        }
+    }
+}
