0e71be9a7b28ee4101e1467ed5419daad710383d
Anna Axelsson <a.axelsson51@gmail.com>
2024-04-03 18:21:24 +0200

    Add functionality for retrieving highest player score

---
+++ Controllers/TopList.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SPAGame.Data;
+
+namespace SPAGame.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TopListController : ControllerBase
+    {
+        private readonly DbContext _context;
+
+        public TopListController(DbContext context)
+        {
+            _context = context;
+        }
+
+        // Retrieving highest score achieved by each user
+
+        [HttpGet]
+        public IActionResult GetHighestPoints()
+        {
+            var highestScores = _context.Scores
+                .Include(score => score.User) 
+                .GroupBy(score => score.UserId)
+                .Select(group => new
+                {
+                    UserId = group.Key,
+                    UserName = group.First().User != null ? group.First().User.UserName : "Unknown", 
+                    HighestPoints = group.Max(score => score.Points)
+                })
+                .ToList();
+
+            return Ok(highestScores);
+        }
+
+
+
+    }
+}
